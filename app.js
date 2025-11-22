3076452015b336fc1687c411e58ec7cefd1c1ae0
TiannaBassaragh <tianna.bassaragh@outlook.com>
2025-11-21 21:45:56 -0500

    Exercise 2 complete: HTML, JS and CSS files created.

---
+++ app.js
@@ -0,0 +1,8 @@
+document.getElementById("searchBtn").addEventListener("click", () => {
+    fetch("superheroes.php")
+        .then(response => response.text())
+        .then(data => {
+            alert(data); 
+        })
+        .catch(error => console.error("Error:", error));
+});
